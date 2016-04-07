c4c805acbae0cf38ff3d1549de83112e2d6f5947
Paul C Roberts <paul@dropbox.com>
2016-04-06 20:52:37 -0700

    Updated, added publications and patents

--- resume.js
+++ resume.js
@@ -2,25 +2,26 @@ const Resume = props => (
   <div className='resume'>
     <div className='row'>
       <Contact {...props.contact} />
-      <h1 className="name">{props.name}</h1>
+      <h1 className='name'>{props.name}</h1>
     </div>
     <Statement name={props.name}statement={props.statement} />
     <Employment jobs={props.jobs} />
     <Education education={props.education} />
+    <Publications publications={props.publications} />
   </div>
 );
 
 const Contact = props => (
-  <div className="contact">
-    <div className="address">
+  <div className='contact'>
+    <div className='address'>
     {
       props.address.split('\n').map((item, index) => <div key={index}>{item}</div>)
     }
     </div>
-    <div className="email">
+    <div className='email'>
       <a href={`mailto:${props.email}`}>{props.email}</a>
     </div>
-    <div className="phone">{props.phone}</div>
+    <div className='phone'>{props.phone}</div>
     <div>
       <a href={`https://${props.github}`}>{props.github}</a>
     </div>
@@ -31,7 +32,7 @@ const Contact = props => (
 );
 
 const Statement = props => (
-  <div className="statement">
+  <div className='statement'>
   {
     props.statement.split('\n').map((item, index) => <div key={index}>{item}</div>)
   }
@@ -45,14 +46,18 @@ const ListOf = React.createClass({
   expand: function(e) {
     e.preventDefault();
     e.stopPropagation();
-    this.setState({hideAfter: this.state.hideAfter + 1 });
+    if (this.props.expandAll) {
+      this.setState({hideAfter: 0});
+    } else {
+      this.setState({hideAfter: this.state.hideAfter + 1 });
+    }
   },
   render: function() {
     const more = this.state.hideAfter &&
                  this.state.hideAfter < this.props.data.length;
     const visible = more ? this.props.data.slice(0, this.state.hideAfter) : this.props.data;
     const expando = more ? (
-                      <div className="expando" onClick={this.expand}>{this.props.more}</div>
+                      <div className='expando' onClick={this.expand}>{this.props.more}</div>
                     ) : null;
     return (
       <div>
@@ -66,14 +71,14 @@ const ListOf = React.createClass({
 });
 
 const Education = props => (
-  <div className="section education">
+  <div className='section education'>
     <h2>Education</h2>
     <ListOf data={props.education} type={EducationItem} />
   </div>
 );
 
 const EducationItem = props => (
-  <div className="educationItem row">
+  <div className='educationItem row'>
     <div className='institution'>
     {
       props.institution.split('\n').map((item, index) => <div key={index}>{item}</div>)
@@ -85,35 +90,35 @@ const EducationItem = props => (
 );
 
 const Employment = props => (
-  <div className="section employment">
+  <div className='section employment'>
     <h2>Professional Experience</h2>
-    <ListOf data={props.jobs} type={Job} hideAfter={2} more="more jobs &gt;&gt;"/>
+    <ListOf data={props.jobs} type={Job} hideAfter={2} more='more jobs &gt;&gt;'/>
   </div>
 );
 
 const Job = props => (
-  <div className="job">
-    <div className="row">
-      <div className="employer">{props.employer}</div>
-      <div className="date">{props.from} - {props.to || 'Present'}</div>
+  <div className='job'>
+    <div className='row'>
+      <div className='employer'>{props.employer}</div>
+      <div className='date'>{props.from} - {props.to || 'Present'}</div>
     </div>
-    <div className="row">
-      <div className="title">{props.title}</div>
-      <div className="technologies">{props.technologies}</div>
+    <div className='row'>
+      <div className='title'>{props.title}</div>
+      <div className='technologies'>{props.technologies}</div>
     </div>
-    <div className="row">
-      <div className="location">{props.location}</div>
+    <div className='row'>
+      <div className='location'>{props.location}</div>
     </div>
-    <div className="row jobExperience">
-        <ListOf data={props.experience} type={Experience} hideAfter={2} more="more details &gt;&gt;&gt;"/>
+    <div className='row jobExperience'>
+        <ListOf data={props.experience} type={Experience} hideAfter={2} more='more details &gt;&gt;&gt;'/>
     </div>
   </div>
 );
 
 const Experience = props => (
-  <div className="experience">
-    <div className="subtitle">{props.subtitle}</div>
-    <div className="dates">{props.dates}</div>
+  <div className='experience'>
+    <div className='subtitle'>{props.subtitle}</div>
+    <div className='dates'>{props.dates}</div>
     {
       props.body.map((item, index) => <ExperienceItem key={index} data={item} /> )
     }
@@ -139,6 +144,29 @@ const ExperienceItem = props => {
     );
   }
 };
+
+const Publications = props => (
+  <div className='section publications'>
+    <h2>Publications, Patents</h2>
+    <ListOf
+      data={props.publications}
+      type={Publication}
+      hideAfter={1}
+      more='show all'
+      expandAll={true}/>
+  </div>
+);
+
+const Publication = props => {
+  const title = props.uri ?  (<a href={props.uri} target='_blank'>{props.title}</a>) : props.title;
+  return (
+    <div className='publication'>
+      <div className='title'>{title}</div>
+      <div className='publisher'>{props.publisher}</div>
+      <div className='date'>{props.date}</div>
+    </div>
+  );
+}
 
 ReactDOM.render(
   <Resume {...resume} />,
